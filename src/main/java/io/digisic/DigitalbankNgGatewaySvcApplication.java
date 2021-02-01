122eb43e8f2106e87b5f61656781230e23ada7f4
Mark Asbury <asburymr@yahoo.com>
2021-02-01 17:30:08 -0600

    Initial Check In

---
+++ src/main/java/io/digisic/DigitalbankNgGatewaySvcApplication.java
@@ -0,0 +1,13 @@
+package io.digisic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalbankNgGatewaySvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalbankNgGatewaySvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/digisic/DigitalbankNgGatewaySvcApplicationTests.java
@@ -0,0 +1,13 @@
+package io.digisic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalbankNgGatewaySvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
